1b0aa151725f9763e6ce820f289f2e6369bf97bd
Helmut Fieres <67054756+HFF-Git@users.noreply.github.com>
2026-05-03 17:51:28 +0200

    Added include files.

--- Twin64-Libraries/Twin64-Processor/T64-Processor.h
+++ Twin64-Libraries/Twin64-Processor/T64-Processor.h
@@ -29,6 +29,7 @@
 #include "T64-System.h"
 #include <thread>
 #include <atomic>
+#include <mutex>
 #include <condition_variable>
 
 //----------------------------------------------------------------------------------------
